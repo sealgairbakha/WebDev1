f2fb05c0b6b7385213e9360aeec1bf4cc6ddc02c
sealgairbakha <147051833+sealgairbakha@users.noreply.github.com>
2026-02-17 16:20:21 +0500

    Add files via upload

---
+++ Lab4/task1/angular tutorial/forms.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
+  `,
+  imports: [FormsModule],
+})
