76c2b8966953c7ca085d9885b823b0c67f143e77
slbaha <bjostar1@gmail.com>
2026-03-31 01:34:00 +0500

    Lab 9

---
+++ Lab9/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('products', ProductViewSet, basename='products')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
